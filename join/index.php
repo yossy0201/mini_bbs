be4cbd9416cd03a359361e9f54e1bb90c0b1945f
yossy0201 <yossy0201msk@gmail.com>
2020-09-29 23:44:06 +0900

    error-check-registerform

--- join/index.php
+++ join/index.php
@@ -2,6 +2,12 @@
 if ($_POST['name'] ==='') {
 		$error['name'] = 'blank';
 }
+if ($_POST['email'] ==='') {
+	$error['email'] = 'blank';
+}
+if ($_POST['password'] ==='') {
+		$error['password'] = 'blank';
+}
 
 ?>
 <!DOCTYPE html>
@@ -36,10 +42,20 @@ if ($_POST['name'] ==='') {
 		</dd>
 		<dt>メールアドレス<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="email" size="35" maxlength="255" value="" />
+        	<input type="text" name="email" size="35" maxlength="255" value="<?php print
+					(htmlspecialchars($_POST['email'],
+					ENT_QUOTES)); ?>" />
+					<?php if ($error['email'] === 'blank'): ?>
+					<p class="error">* メールアドレスを入力してください</p>
+					<?php endif; ?>
 		<dt>パスワード<span class="required">必須</span></dt>
 		<dd>
-        	<input type="password" name="password" size="10" maxlength="20" value="" />
+        	<input type="password" name="password" size="10" maxlength="20" value="<?php print
+					(htmlspecialchars($_POST['password'],
+					ENT_QUOTES)); ?>" />
+					<?php if ($error['password'] === 'blank'): ?>
+					<p class="error">* パスワードを入力してください</p>
+					<?php endif; ?>
         </dd>
 		<dt>写真など</dt>
 		<dd>
