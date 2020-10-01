49e3a7015f6ce643e816f1a9fe133921aa07426e
yossy0201 <yossy0201msk@gmail.com>
2020-10-01 22:54:21 +0900

    session

--- join/index.php
+++ join/index.php
@@ -1,21 +1,24 @@
 <?php
+session_start();
+
 if (!empty($_POST)) {
 	if ($_POST['name'] ==='') {
-		$error['name'] = 'blank';
+			$error['name'] = 'blank';
 	}
 	if ($_POST['email'] ==='') {
-		$error['email'] = 'blank';
+			$error['email'] = 'blank';
 	}
 	if (strlen($_POST['password']) < 4) {
 			$error['password'] = 'length';
 	}
 	if ($_POST['password'] ==='') {
-		$error['password'] = 'blank';
+			$error['password'] = 'blank';
 	}
 
 	if (empty($error)) {
-		header('location: check.php');
-		exit();
+			$_SESSION['join'] = $_POST;
+			header('Location: check.php');
+			exit();
 	}
 }
 
